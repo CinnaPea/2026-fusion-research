1dcd17403ee4cc1d464192072183e70087fe0a78
Pea Shooter <hakguy6@gmail.com>
2026-07-21 18:13:52 +0700

    Removed some build files

--- build/Desktop_Qt_6_11_1_shared_MinGW_w64_UCRT64_MSYS2_Debug/.qt/deploy_Visual_Thermal_Concept_9c01dc3704.cmake
+++
@@ -1,8 +0,0 @@
-include("F:/qtRoadmap/Visual_Thermal_Concept/build/Desktop_Qt_6_11_1_shared_MinGW_w64_UCRT64_MSYS2_Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Visual_Thermal_Concept-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "F:/qtRoadmap/Visual_Thermal_Concept/build/Desktop_Qt_6_11_1_shared_MinGW_w64_UCRT64_MSYS2_Debug/Visual_Thermal_Concept.exe"
-    GENERATE_QT_CONF
-)
